ddefa2523c471e0e7a1cf17630ce3b35d1357c94
Prokop <prokop.schield@protonmail.com>
2022-11-21 13:52:11 +0100

    fix: do not oversaturate streams

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import http from 'http';
 import nsblob from 'nsblob';
 import { PassThrough, Readable, Writable } from 'stream';
 
+const CHUNK_LENGTH = 0x1000;
+
 export async function store(
 	stream: fs.ReadStream | http.IncomingMessage | Readable
 ): Promise<string> {
@@ -13,11 +15,13 @@ export async function store(
 	stream.on('data', (chunk: string | Buffer) => {
 		buffer = Buffer.concat([buffer, Buffer.from(chunk)]);
 
-		while (buffer.length >= 0x10000) {
-			promises.push(nsblob.store(buffer.subarray(0, 0x10000)));
-			buffer = buffer.subarray(0x10000);
+		while (buffer.length >= CHUNK_LENGTH) {
+			promises.push(nsblob.store(buffer.subarray(0, CHUNK_LENGTH)));
+			buffer = buffer.subarray(CHUNK_LENGTH);
 		}
 	});
+
+	stream.resume();
 
 	await new Promise((resolve) => stream.on('end', resolve));
 
@@ -38,11 +42,35 @@ export async function saturate(
 ) {
 	const hashes = (await nsblob.fetch(hash)).toString('hex');
 
+	let streamEnded = false;
+	let cb = () => {};
+
+	stream.on('close', () => ((streamEnded = true), cb()));
+	stream.on('error', () => ((streamEnded = true), cb()));
+	stream.on('finish', () => ((streamEnded = true), cb()));
+
 	for (let i = 0; i < hashes.length; i += 64) {
-		stream.write(await nsblob.fetch(hashes.slice(i, i + 64)));
+		if (!stream.write(await nsblob.fetch(hashes.slice(i, i + 64)))) {
+			await new Promise<void>((resolve) => {
+				let resolved = false;
+
+				cb = () => {
+					if (!resolved) {
+						resolved = true;
+						resolve();
+					}
+				};
+
+				stream.on('drain', cb);
+			});
+		}
+
+		if (streamEnded) {
+			return stream.end();
+		}
 	}
 
-	stream.end();
+	return stream.end();
 }
 
 export function fetch(hash: string): PassThrough {
